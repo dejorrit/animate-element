d30a529d520ec567a1648349ff8389d07a15e684
Jorrit Tinholt <dejorrit@gmail.com>
2018-04-09 17:12:56 +0200

    New dist files

--- dist/animate-element.js
+++ dist/animate-element.js
@@ -9,15 +9,15 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var AnimateElement = function () {
-	function AnimateElement(element, properties, duration) {
+	function AnimateElement(element, properties, options) {
 		var _this = this;
 
 		_classCallCheck(this, AnimateElement);
 
 		this.element = element;
 		this.properties = properties;
-		this.duration = duration;
-
+		this.duration = options.duration || 250;
+		this.easing = options.easing || false;
 		this.starttime = null;
 		this.startStyles = window.getComputedStyle(element);
 
@@ -34,15 +34,16 @@ var AnimateElement = function () {
 
 			timestamp = timestamp || new Date().getTime();
 
-			var runtime = timestamp - this.starttime,
-			    progress = Math.min(runtime / this.duration, 1);
+			var runtime = timestamp - this.starttime;
+			var progress = Math.min(runtime / this.duration, 1);
 
 			Object.entries(this.properties).forEach(function (property) {
-				var key = property[0],
-				    val = property[1],
-				    unit = _this2.startStyles[key].indexOf('px') !== -1 ? 'px' : '',
-				    startValue = parseInt(_this2.startStyles[key], 10),
-				    newValue = startValue + (val - startValue) * ease(progress);
+				var key = property[0];
+				var val = property[1];
+				var unit = _this2.startStyles[key].indexOf('px') !== -1 ? 'px' : '';
+
+				var startValue = parseInt(_this2.startStyles[key], 10);
+				var newValue = startValue + ((val - startValue) * _this2.easing ? ease(progress) : progress);
 
 				_this2.element.style[key] = newValue + unit;
 			});
