b70381c0e507b4f28b868bfcdae1f6ddbf18070e
Jorrit Tinholt <dejorrit@gmail.com>
2018-04-09 16:56:04 +0200

    Source code

---
+++ src/animate-element.js
@@ -0,0 +1,44 @@
+export default class AnimateElement {
+
+	constructor(element, properties, duration) {
+		this.element     = element;
+		this.properties  = properties;
+		this.duration    = duration;
+		this.starttime   = null;
+		this.startStyles = window.getComputedStyle(element);
+
+		requestAnimationFrame(timestamp => {
+			this.starttime = timestamp || new Date().getTime();
+			this.animate(timestamp);
+		});
+	}
+
+	animate(timestamp) {
+		timestamp = timestamp || new Date().getTime();
+
+		let runtime = timestamp - this.starttime;
+		let progress = Math.min(runtime / this.duration, 1);
+
+		Object.entries(this.properties).forEach(property => {
+			let key  = property[0];
+			let val  = property[1];
+			let unit = this.startStyles[key].indexOf('px') !== -1 ? 'px' : '';
+
+			let startValue = parseInt(this.startStyles[key], 10);
+			let newValue   = startValue + ((val - startValue) * ease(progress));
+
+			this.element.style[key] = newValue + unit;
+		});
+
+		if (runtime < this.duration) {
+			requestAnimationFrame(timestamp => {
+				this.animate(timestamp);
+			});
+		}
+	}
+
+}
+
+function ease(n) {
+	return .5 * (1 - Math.cos(Math.PI * n));
+}
